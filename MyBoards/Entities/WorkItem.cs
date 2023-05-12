99dcc545f17c9a634b187937b8433f2c7b989f5e
marcsien <marcsien@gmail.com>
2023-05-12 07:26:00 +0200

    Add entities definitions

---
+++ MyBoards/Entities/WorkItem.cs
@@ -0,0 +1,19 @@
+﻿namespace MyBoards.Entities
+{
+    public class WorkItem
+    {
+        public string State { get; set; }
+        public string Area { get; set; }
+        public string IterationPath { get; set; }
+        public int Priority { get; set; }
+        //Epic
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        //Issue
+        public decimal Effort { get; set; }
+        //Task
+        public string Activity { get; set; }
+        public decimal RemainingWork { get; set; }
+        public string Type { get; set; }
+    }
+}
