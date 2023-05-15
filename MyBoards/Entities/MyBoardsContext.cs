8fc523f48809a6393b0b7c301fa1a9fc5ab1a1e9
marcsien <marcsien@gmail.com>
2023-05-15 07:53:54 +0200

    DBContext + Connection string

---
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=DESKTOP-HELIKOP;Database=EfBoardsDb;Trusted_Connection=True");
+        //}
+    }
+}
--- MyBoards/Program.cs
+++ MyBoards/Program.cs
@@ -1,9 +1,16 @@
+using MyBoards.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyBoardsContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsConnectionString"))
+    );
 
 var app = builder.Build();
 
