cd2c8c3fe2e34fde2530a158233a86c98d97760e
marcsien <marcsien@gmail.com>
2023-05-17 20:15:03 +0200

    set example default values for entities

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -45,6 +45,13 @@ namespace MyBoards.Entities
                 eb.Property(wi => wi.RemainingWork).HasPrecision(14,2);
                 eb.Property(wi => wi.State).IsRequired();
                 eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
+                eb.Property(wi => wi.Priority).HasDefaultValue(1);
+            });
+
+            modelBuilder.Entity<Comment>(eb =>
+            {
+                eb.Property(wi => wi.CreatedDate).HasDefaultValueSql("getutcdate()");
+                eb.Property(wi => wi.UpdatedDate).ValueGeneratedOnUpdate();
             });
 
         }
